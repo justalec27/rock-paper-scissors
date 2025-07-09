6dc843442e32617bf28eea36496172713bfb8dd6
Alec <215294797+justalec27@users.noreply.github.com>
2025-07-09 15:53:57 +0200

    create function for human choice
    user is allowed to input his choice: rock, paper or scissors.

--- javascript.js
+++ javascript.js
@@ -11,15 +11,32 @@ console.log("Hello world")
 
 function getComputerChoice(){
     let randomNumber = Math.random()
-    console.log(randomNumber) //remove later
+    console.log(randomNumber) // delete later
     if (randomNumber <= 0.33){
         return "rock";
     }  else if (randomNumber > 0.33 && randomNumber <= 0.66){
         return "paper";
     }  else if (randomNumber > 0.66 ){
-        return "scissors"
+        return "scissors";
     }
 
 }
 
-console.log(getComputerChoice())
+console.log(`The computer chooses: ${getComputerChoice()}`) // delete later
+
+
+/* Get human choice
+    1. Create a function getHumanChoice
+    2. create a variable for string data type
+    3. prompt the user to enter a choice; rock, paper or scissors and store that in the variabel
+    4. return the variable value
+*/
+
+
+function getHumanChoice(){
+    let humanChoice = prompt("What do you choose? Rock, paper or scissors: ").toLowerCase();
+    return humanChoice;
+
+}
+
+console.log(`The user chooses: ${getHumanChoice()}`)    
